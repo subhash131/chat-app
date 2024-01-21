246ef5c8657fe43a0e6315363c79bd14a0e3c464
subhash131 <subhashnayak131@gmail.com>
2024-01-22 02:01:03 +0530

    create sponsors page

--- apps/web/app/sponsors/page.tsx
+++ apps/web/app/sponsors/page.tsx
@@ -1,10 +1,61 @@
 import React from "react";
 import { Card } from "../../components/sponsors/Card";
+import styles from "./sponsors.module.css";
+import RecentSponsors from "../../components/sponsors/RecentSponsorsCard";
+import Titlebar from "../../components/common/Titlebar";
 
 const page = () => {
   return (
-    <div>
-      <Card />
+    <div className={styles.container}>
+      <div className={styles.sponsors_container}>
+        <div className={styles.sponsors_search}>Search</div>
+        <div className={styles.sponsors_list}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+      <div className={styles.recent_sponsors_container}>
+        <Titlebar name="Recent Sponsors" component="" />
+        <div className={styles.recent_sponsors_list}>
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+          <RecentSponsors />
+        </div>
+      </div>
     </div>
   );
 };
