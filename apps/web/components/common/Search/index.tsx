234049509df8435fce1bd77d56333233d794b386
subhash131 <subhashnayak131@gmail.com>
2024-01-22 03:41:56 +0530

    create reusable component: SearchComp

--- apps/web/components/common/Search/index.tsx
+++ apps/web/components/common/Search/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./search.module.css";
+import { Search } from "lucide-react";
 
-const index = () => {
-  return <div>index</div>;
+const SearchComp = () => {
+  return (
+    <div className={styles.input_container}>
+      <Search strokeWidth={1.5} width={18} />
+      <input className={styles.input} placeholder="Search" />
+    </div>
+  );
 };
 
-export default index;
+export default SearchComp;
