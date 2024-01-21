2a4b5d6856f0b3fcc9719d849ccddf47a34c1a9e
subhash131 <subhashnayak131@gmail.com>
2024-01-21 22:29:28 +0530

    add card component

--- apps/web/app/sponsors/page.tsx
+++ apps/web/app/sponsors/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Card } from "../../components/sponsors/Card";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <Card />
+    </div>
+  );
 };
 
 export default page;
