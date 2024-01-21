6e1f478451290dfd218c10a1d42e8c08048798b3
subhash131 <subhashnayak131@gmail.com>
2024-01-22 02:05:42 +0530

    restructure folder: move titlebar component to common folder

---
+++ apps/web/components/common/Titlebar/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./common.module.css";
+
+const Titlebar = ({
+  name,
+  component,
+}: {
+  name: string;
+  component: React.ReactNode;
+}) => {
+  return (
+    <div className={styles.title_bar}>
+      <p>{name}</p>
+      {component}
+    </div>
+  );
+};
+
+export default Titlebar;
